fe45f0408fe0bef03ea402c6049c420f17440b31
LucasTonny <lucashsuana@gmail.com>
2026-08-21 15:57:54 -0300

    teste

---
+++ src/main/java/br/com/projetointerdisciplinar/copadomundo/copadomundo/CopadomundoApplication.java
@@ -0,0 +1,13 @@
+package br.com.projetointerdisciplinar.copadomundo.copadomundo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CopadomundoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CopadomundoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/projetointerdisciplinar/copadomundo/copadomundo/CopadomundoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projetointerdisciplinar.copadomundo.copadomundo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CopadomundoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
